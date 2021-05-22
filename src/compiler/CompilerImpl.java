323f313f20a5d56e57284ac2c52b9d0b5dfc7f30
Miljana <mstanic5819rn@raf.rs>
2021-05-22 23:55:19 +0200

    Implementirana konkatenacija stringa koji korisnik unosi unutar textBox-a.

--- src/compiler/CompilerImpl.java
+++ src/compiler/CompilerImpl.java
@@ -1,65 +1,55 @@
 package compiler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CompilerImpl implements Compiler{
 
+    private Query query;
+    private List<Query> parts;
+
     @Override
     public void compile(String s) {
         System.out.println(s);
-        String a;
         String query = s;
+        String newQuery;
         String[] varCheck;
         String[] functions;
-        String[] words;
-        String[] finalWords;
-        String temp = "";
-        ArrayList<String> input = new ArrayList<>();
 
         varCheck = query.split("new ");
-        //System.out.println(varCheck[varCheck.length-1]);
 
         functions = varCheck[varCheck.length-1].split("\\).");
-        //System.out.println(rightSide);
-        //System.out.println(functions[functions.length-1]);
 
         for(int i=0; i<functions.length; i++){
-            System.out.println(functions[i]);
-
+            devide(functions[i]);
         }
-        //System.out.println(temp);
-
-        //words = temp.split("\"");
-        //for(int i=0; i<words.length; i++){
-            //if(words[i].charAt(i) == '('){
-
-            //}
-            //words[i].replaceAll("\\(\\)"," ");
-            //(String)words[i].split("\(");
-           //System.out.println(words[i]);
-//           a = words[i];
-//           a.replaceAll("()"," ");
-//            System.out.println(a);
-//           temp = words[i];
-//           if(words[i].equals("(")){
-//                words[i].replace("("," ");
-//            }else if(words[i].equals(")")){
-//                words[i].replace(")"," ");
-//            }
-//            System.out.println(words[i]);
- //       }
-        //a.replaceAll("()"," ");
-         //a.replaceAll("()","");
-
-
-//        finalWords = temp.split("");
-//        for (int i = 0 ; i<finalWords.length;i++){
-//            System.out.println(finalWords[i]);
-//        }
-
-
-
-
+        newQuery = makeSQLQuery();
+        System.out.println(newQuery);
     }
+
+    public void devide(String input){
+        String[] probica1;
+        String[] probica2;
+
+        probica1 = input.split("\\(");
+        probica2 = probica1[1].split("[,]");
+
+        query = new Query(probica1[0], probica2);
+        query.queryDivide();
+    }
+
+    public String makeSQLQuery(){
+        parts = query.getAllPartsOfQuery();
+        String funName;
+        String out = "";
+        for (Query part: parts) {
+           funName = part.getFunctionName();
+           out +=  funName.toLowerCase() + " " + part.toString();
+            //out +=  funName.toLowerCase() + " " + Arrays.toString(part.getArguments()) + " ";
+        }
+        query.removeAllPartsOfQuery();
+        return out;
+    }
+
 }
---
+++ src/compiler/Query.java
@@ -0,0 +1,58 @@
+package compiler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Query {
+
+    private String functionName;
+    private String[] arguments;
+    private static List<Query> allPartsOfQuery = new ArrayList<>();
+
+    public Query(String functionName, String[] arguments) {
+        this.functionName = functionName;
+        this.arguments = arguments;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String[] getArguments() {
+        return arguments;
+    }
+
+    public void queryDivide(){
+        int size = arguments.length;
+
+        for (int i=0; i< arguments.length; i++){
+            if(arguments[i].endsWith("\"") || arguments[i].startsWith("\"")){
+                String temp = arguments[i];
+                arguments[i] = temp.replace("\"", "");
+            }
+        }
+        if(arguments[size-1].endsWith(")")){
+            String temp = arguments[size-1];
+            arguments[size-1] = temp.replace(")", "");
+        }
+        allPartsOfQuery.add(this);
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for(int i=0 ; i<arguments.length; i++){
+            s += arguments[i] + " ";
+        }
+        return s;
+    }
+
+    public static List<Query> getAllPartsOfQuery() {
+        return allPartsOfQuery;
+    }
+
+    public static void removeAllPartsOfQuery() {
+        allPartsOfQuery.clear();
+    }
+}
