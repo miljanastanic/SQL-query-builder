70d1cb861a4c8e4184aafbea1f90a25fd72a108a
Miljana <mstanic5819rn@raf.rs>
2021-05-25 15:02:02 +0200

    Implementacija dokumentacije.

--- src/compiler/CompilerImpl.java
+++ src/compiler/CompilerImpl.java
@@ -49,34 +49,114 @@ public class CompilerImpl implements Compiler{
 
            funName = part.getFunctionName();
 
+           //1.Upit nad tabelom
            if(funName.equalsIgnoreCase("query")){
                System.out.println("ovo radi 1");
                part.setFunctionName("FROM");
-               out +=  part.getFunctionName() + " " + part.toString();
-               //funName = part.getFunctionName();
-               //System.out.println(funName);
+               out +=  part.getFunctionName() + " " + part.toString() + ".";
            }
+           //2.Projekcija
            if(funName.equalsIgnoreCase("select")){
                 if(part.getArguments().length == 0){
                     part.setFunctionName("SELECT *");
                 }else{
                     part.setFunctionName("SELECT");
                 }
-                out +=  part.getFunctionName() + " " + part.toString();
+                out +=  part.getFunctionName() + " " + part.toString() + ".";
            }
+           //3.Sortiranje
            if(funName.equalsIgnoreCase("orderby")){
                 part.setFunctionName("ORDER BY");
-                out +=  part.getFunctionName() + " " + part.toString();
+                out +=  part.getFunctionName() + " " + part.toString() + ".";
            }
            if(funName.equalsIgnoreCase("orderbydesc")){
                 part.setFunctionName("ORDER BY");
-                out +=  part.getFunctionName() + " " + part.toString() + "DESC" + " ";
+                out +=  part.getFunctionName() + " " + part.toString() + "DESC" + ".";
            }
+           //4.Filtriranje
            if(funName.equalsIgnoreCase("where")){
-               out +=  part.getFunctionName() + " " + part.toString();
+               out +=  part.getFunctionName() + " " + part.toString() + ".";
            }
-           //out +=  part.getFunctionName() + " " + part.toString();
-           //out +=  funName.toLowerCase() + " " + Arrays.toString(part.getArguments()) + " ";
+           if(funName.equalsIgnoreCase("orwhere")){
+               part.setFunctionName("WHERE");
+               part.setFunctionName("WHERE");
+               out +=  part.getFunctionName() + " " + part.getArguments()[0] + " " + "OR" + " "+ part.getArguments()[1] + " " + part.getArguments()[2]+ ".";
+           }
+           if(funName.equalsIgnoreCase("andwhere")){
+               part.setFunctionName("WHERE");
+               out +=  part.getFunctionName() + " " + part.getArguments()[0] + " " + "AND" + " " + part.getArguments()[1] + " " + part.getArguments()[2]+ ".";
+           }
+           if(funName.equalsIgnoreCase("wherebetween")){
+               part.setFunctionName("WHERE");
+               out +=  part.getFunctionName() + " " + part.getArguments()[0] + " "  + "BETWEEN" + " "  + part.getArguments()[1]+ " " + "AND" + " " + part.getArguments()[2] + ".";
+           }
+           if(funName.equalsIgnoreCase("wherein")){
+               part.setFunctionName("WHERE");
+               out +=  part.getFunctionName() + " "+ part.getArguments()[0] + " " +"IN"+ " " + "(";
+               for(int j=1; j < part.getArguments().length; j++){
+                   out+=part.getArguments()[j];
+                   if(!(j == (part.getArguments().length)-1)){
+                       out+=",";
+                   }
+               }
+               out+=")" + ".";
+           }
+           //6.Stringovne operacije (where department_name like 'S%')
+            if(funName.equalsIgnoreCase("whereendswith")) {
+                part.setFunctionName("WHERE");
+                out += part.getFunctionName() + " " + part.getArguments()[0] + " " + "like" + " " + "'" + "%" + part.getArguments()[1] + "'" + ".";
+            }
+            if(funName.equalsIgnoreCase("wherestartswith")) {
+                part.setFunctionName("WHERE");
+                out += part.getFunctionName() + " " + part.getArguments()[0] + " " + "like" + " " + "'" + part.getArguments()[1] + "%" + "'" + ".";
+            }
+            if(funName.equalsIgnoreCase("wherecontains")) {
+                part.setFunctionName("WHERE");
+                out += part.getFunctionName() + " " + part.getArguments()[0] + " " + "like" + " " + "'"+ "%" + part.getArguments()[1] + "%" + "'" + ".";
+            }
+            //7.Funkcije agregacije
+            if(funName.equalsIgnoreCase("avg")){
+                part.setFunctionName("avg");
+                out += part.getFunctionName() + "(" + part.getArguments() + ")" + ".";
+            }
+            if(funName.equalsIgnoreCase("count")){
+                part.setFunctionName("count");
+                out += part.getFunctionName() + "(" + part.getArguments() + ")" + ".";
+            }
+            if(funName.equalsIgnoreCase("min")){
+                part.setFunctionName("min");
+                out += part.getFunctionName() + "(" + part.getArguments() + ")" + ".";
+            }
+            if(funName.equalsIgnoreCase("max")){
+                part.setFunctionName("max");
+                out += part.getFunctionName() + "(" + part.getArguments() + ")" + ".";
+            }
+            if(funName.equalsIgnoreCase("groupby")){
+                part.setFunctionName("GROUP BY");
+                out += part.getFunctionName() + " ";
+                for(int j=1; j < part.getArguments().length; j++){
+                    out+=part.getArguments()[j];
+                    if(!(j == (part.getArguments().length)-1)){
+                        out+= " " + ",";
+                    }
+                    out+=".";
+                }
+            }
+            if(funName.equalsIgnoreCase("having")){
+                part.setFunctionName("HAVING");
+                out +=  part.getFunctionName() + " " + part.getArguments()[0] + " " + part.getArguments()[1]+ " " + part.getArguments()[2] + ".";
+            }
+            if(funName.equalsIgnoreCase("andhaving")){
+                part.setFunctionName("HAVING");
+                out +=  part.getFunctionName() + " " + part.getArguments()[0] + " " + "AND" + " " + part.getArguments()[1] + " " + part.getArguments()[2]+ ".";
+            }
+            if(funName.equalsIgnoreCase("orhaving")){
+                part.setFunctionName("HAVING");
+                out +=  part.getFunctionName() + " " + part.getArguments()[0] + " " + "OR" + " " + part.getArguments()[1] + " " + part.getArguments()[2]+ ".";
+            }
+            //OSTALI SU 8.PODUPITI i 5.
+            //out +=  part.getFunctionName() + " " + part.toString();
+            //out +=  funName.toLowerCase() + " " + Arrays.toString(part.getArguments()) + " ";
         }
 
         query.removeAllPartsOfQuery();
